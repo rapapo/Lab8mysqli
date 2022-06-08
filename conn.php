1bbef073ca94f5a4e7194ce902747b82c88956c6
rapapo <asyraf.akif91k@gmail.com>
2022-06-08 16:47:33 +0800

    2

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
 define ('db_user', 'root');
 define ( 'db_password','');
 define ('db_host', 'localhost');
-define ( 'db_name", 'lab7');
+define ( 'db_name', 'lab7');
 
 $conn = mysqli_connect (db_host, db_user, db_password, db_name);
 
