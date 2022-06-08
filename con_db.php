e943cf7bfab55b5734a0d05c315b404aec26b8d6
rapapo <asyraf.akif91k@gmail.com>
2022-06-08 16:18:00 +0800

    sec com

--- con_db.php
+++ con_db.php
@@ -1,16 +1,17 @@
 <?php
-define('db_user', 'root') ;
-define('db_password', '');
-define('db_host', 'localhost');
-define('db_name', 'registration');
+define('db_user'),('root');
+define('db_passsword','');
+define('db_host','localhost');
+define('db_name','registration');
 
-$conn = mysqli_connect (db_host, db_user, db_password, db_name);
-$result = mysqli_query ($conn, "select * from user");
-while ($row = mysqli_fetch_array ($result)){
-    foreach ($row as $key=>$value) {
-    echo "$key = $value <br>\n";
+$conn=mysql_connect (db_host, db_user, db_password, db_name);
+$result=mysql_query($conn,"select*from user");
+while($row=mysql_fetch_array($result)){
+    foreeach($row as $key=>$value){
+        echo "$key=$value <br>\n";
     }
 }
-mysqli_free_result ($result);
-mysqli_close ($conn);
+
+mysql_free_result($result);
+mysql_close($conn);
 ?>
