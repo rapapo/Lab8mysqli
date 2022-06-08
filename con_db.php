3fa51d0e0fbf86cce224bf34df03c4973b1733ea
rapapo <asyraf.akif91k@gmail.com>
2022-06-08 16:22:55 +0800

    5

--- con_db.php
+++ con_db.php
@@ -1,17 +1,17 @@
 <?php
-define('db_user'),('root');
+define('db_user','root');
 define('db_passsword', null);
 define('db_host','localhost');
 define('db_name','registration');
 
-$conn=mysql_connect (db_host, db_user, db_password, db_name);
-$result=mysql_query($conn,"select*from user");
-while($row=mysql_fetch_array($result)){
-    foreeach($row as $key=>$value){
+$conn=mysqli_connect (db_host, db_user, db_passsword, db_name);
+$result=mysqli_query($conn,"select*from user");
+while($row=mysqli_fetch_array($result)){
+    foreach($row as $key=>$value){
         echo "$key=$value <br>\n";
     }
 }
 
-mysql_free_result($result);
-mysql_close($conn);
+mysqli_free_result($result);
+mysqli_close($conn);
 ?>
