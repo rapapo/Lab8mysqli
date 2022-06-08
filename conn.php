5ac536affabfc5668989c1d9e6d27aaf8363787f
rapapo <asyraf.akif91k@gmail.com>
2022-06-08 16:49:56 +0800

    3

--- conn.php
+++ conn.php
@@ -8,6 +8,6 @@ $conn = mysqli_connect (db_host, db_user, db_password, db_name);
 
 if (mysqli_connect_errno ())
 {
-    echo Failed to connect to MysQL: " - mysqli_connect_error ();
+    echo "Failed to connect to MysQL: " . mysqli_connect_error ();
 }
 ?>
